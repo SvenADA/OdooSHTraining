dca9f4103aa9647095caf8371dfe837ce9c2fa2b
Sven <sven.seemann@adarobotics.com>
2022-02-21 04:27:16 +0000

    [ADD] Basic module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+    
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage training""",
+    
+    'descrption': """
+        Academy module to manage Training: 
+        - Courses
+        - Sessions
+        - Attendances
+    """,
+    
+    'Author': 'Sven',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+}
